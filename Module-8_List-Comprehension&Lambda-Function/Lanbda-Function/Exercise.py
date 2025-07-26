1fb2dff991a108d8f39fa0c03dfa4f0bc9700242
Harsh-adey <161951722+Harsh-adey@users.noreply.github.com>
2025-07-26 13:16:40 +0530

    Update and rename cube.py to Exercise.py

--- Module-8_List-Comprehension&Lambda-Function/Lanbda-Function/Exercise.py
+++ Module-8_List-Comprehension&Lambda-Function/Lanbda-Function/Exercise.py
@@ -1,6 +1,8 @@
 #1)cubes every number in the given list list_1 = [1,2,3,4,5,6,7,8,9] using lambda function 
-list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 cubed_list = list(map(lambda x: x**3, list_1))
 
 print(cubed_list)
+
+
